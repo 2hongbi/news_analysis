852b7f83b3e5a5e09d4518fe8ce6f86fcbd5ee26
hongbi-lee <foxgirin950@gmail.com>
2021-10-08 20:27:27 +0900

    move excel files to data/comments/ directory and modify data_utils.py to assemble youtube comments

--- src/data_utils.py
+++ src/data_utils.py
@@ -2,18 +2,22 @@ import pandas as pd
 import os.path
 
 
-def extract_comments(file, output):
+# xlsx 파일 댓글 내용만 추출하여 csv 파일 만드는 함수
+def extract_comments(file, output, type='nv'):
     df = pd.read_excel(file)
+    column = ['내용']
+    if type == 'yt':
+        column = ['comment']
     if os.path.isfile(output):
-        df.to_csv(output, sep=',', na_rep='NaN', columns=['내용'], index=False, mode='a', encoding='utf-8-sig',
+        df.to_csv(output, sep=',', na_rep='NaN', columns=column, index=False, mode='a', encoding='utf-8-sig',
                   header=False)
     else:
-        df.to_csv(output, sep=',', na_rep='NaN', columns=['내용'], index=False, mode='w', encoding='utf-8-sig',
+        df.to_csv(output, sep=',', na_rep='NaN', columns=column, index=False, mode='w', encoding='utf-8-sig',
                   header=False)
 
 
 if __name__ == '__main__':
-    file_list = [
+    nv_file_list = [
         '머니투데이_아프간난민을미군기지로.xlsx',
         'news1_아프간난민수용보도에찬반뜨거워.xlsx',
         '서울신문_전세계아프간난민딜레마.xlsx',
@@ -22,5 +26,11 @@ if __name__ == '__main__':
         '한국일보_한국은선진국난민수용선택의문제아니다.xlsx'
     ]
 
-    for file in file_list:
-        extract_comments('./../data/'+file, './../data/comments.csv')
+    for file in nv_file_list:
+        extract_comments('./../data/comments/'+file, './../data/all_nanmin_comments.csv')
+        continue
+
+    yt_file_list = ['channelA_news.xlsx', 'mbc_news.xlsx']
+
+    for file in yt_file_list:
+        extract_comments('./../data/comments/'+file, './../data/all_nanmin_comments.csv', type='yt')
